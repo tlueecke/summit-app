bbc8cdd3e0f796e612d2797d7eb336826d646ddd
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2017-03-19 14:47:01 +0100

    whitespace fixes

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 // Rgister event listener for the 'push' event.
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
-    
+
     var  data = event.data.json();
     console.log(JSON.stringify(data));
 
     // Keep the service worker alive until the notification is created.
-    event.waitUntil(	
-	self.registration.showNotification(data.title, {
-	    body: data.body,
-	    icon: data.icon,
-	    vibrate: data.vibrate
-	})
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: data.icon,
+            vibrate: data.vibrate
+        })
     );
 });
 
