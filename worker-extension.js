9dfa57b43b894280f81c5e996468f90be99d2e99
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2017-03-19 22:10:45 +0100

    fix

--- worker-extension.js
+++ worker-extension.js
@@ -36,6 +36,6 @@ self.addEventListener('push', function(event) {
     );
 });
 
-f.addEventListener('notificationclick', function(event) {
+self.addEventListener('notificationclick', function(event) {
     event.notification.close();
 });
