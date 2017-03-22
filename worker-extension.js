e704460fc108b41c80678f47d807f8dc295b25a6
mmatczak <marek.matczak@capgemini.com>
2017-03-22 06:08:18 +0100

    Push-Notification suppport added

--- worker-extension.js
+++ worker-extension.js
@@ -17,3 +17,24 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  var  data = event.data.json();
+  console.log(JSON.stringify(data));
+
+  // Keep the service worker alive until the notification is created.
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon,
+      badge: data.badge,
+      vibrate: data.vibrate
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+});
